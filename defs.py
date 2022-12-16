ce023f134748076f711878c2d7b49094f511e441
Pericles Feitoza <periclesanfe@gmail.com>
2022-12-15 22:09:17 -0300

    criação de defs.py
    criei o modulo defs.py para armazenar algumas funções repetitivas no loop do jogo

---
+++ defs.py
@@ -0,0 +1,12 @@
+import pygame
+import config as cf
+
+#Essa função vai reiniciar os parâmetros do jogo
+def reiniciar_jogo():
+    global contador
+    contador = 0
+    cf.rua_numero = 0
+    cf.morreu = False
+    cf.carro_pos_x = 555
+    cf.carro_pos_y = 556
+    pygame.display.update()
