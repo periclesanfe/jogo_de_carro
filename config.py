64c9076bd988103fa5f5188e4e1a623575605517
Pericles Feitoza <periclesanfe@gmail.com>
2022-12-16 22:38:21 -0300

    controle de qualidade

--- config.py
+++ config.py
@@ -1,12 +1,21 @@
 import pygame
 import os
-import random
+import carro_obstaculo as co
+import carro_player as cp
 
-#Este comando inicializa as funções e variáveis da biblioteca pygame
+
 pygame.init()
-pygame.mixer.init()          #Iniciando o módulo mixer
 
-#Criando as varáveis do jogo
+
+#DIRETÓRIOS
+
+diretorio_principal = os.path.dirname(__file__)                   #Este diretório é o principal, trabalha com o arquivo em si
+diretorio_imagens = os.path.join(diretorio_principal, 'sprites')  #Este diretório é responsavel pelas sprites do jogo
+diretorio_sons = os.path.join(diretorio_principal, 'songs')       #Este diretório é responsavel pelos sons do jogo(game)
+
+
+#VARIÁVEIS GLOBAIS DO JOGO
+
 ALTURA = 644
 LARGURA = 820
 BRANCO = (255,255,255)
@@ -21,38 +30,55 @@ carro_pos_y = 556
 car_pos_x = 555
 car_pos_y = -120
 rua_numero = 0
+contador = 0
+relogio = pygame.time.Clock() 
 
 
-diretorio_principal = os.path.dirname(__file__)                   #Este diretório é o principal, trabalha com o arquivo em si
-diretorio_imagens = os.path.join(diretorio_principal, 'sprites')  #Este diretório é responsavel pelas sprites do jogo
-diretorio_sons = os.path.join(diretorio_principal, 'songs')       #Este diretório é responsavel pelos sons do jogo(game)
+#MENSAGENS
+
+mensagem_morreu = f'Você morreu, aperte R para reiniciar'
+pontuacao = f'{str(contador).zfill(2)}'
+quadro_de_pontuacao = fonte.render(pontuacao, True, BRANCO, PRETO)
 
 
-musica_partida = pygame.mixer.music.load(os.path.join(diretorio_sons, './audios_convertidos/musica_partida.mp3'))
-musica_partida = pygame.mixer.music.set_volume(0.25)
+#TELAS              
 
-'''musica_menu = pygame.mixer.music.load(os.path.join(diretorio_sons, './audios_convertidos/musica_menu.mp3'))
-pygame.mixer.w
-musica_partida.set_volume(1)
-
-colisao_buraco = pygame.mixer.music.load(os.path.join(diretorio_sons, 'colisao_buraco.wav'))
-colisao_buraco.set_volume(1)
-
-colisão_carros = pygame.mixer.music.load(os.path.join(diretorio_sons, 'colisão_carros.wav'))
-colisão_carros.set_volume(1)
-
-colisao_troncos = pygame.mixer.music.load(os.path.join(diretorio_sons, 'colisao_troncos.wav'))
-colisao_troncos.set_volume(1)                    
-    '''
-
-#Criada uma variável (objeto) e a função cria uma janela
 tela = pygame.display.set_mode((LARGURA, ALTURA))
 
-#Este comando insere um nome ao jogo
-pygame.display.set_caption('jogo_de_carro') 
+tela_reiniciar = fonte.render(mensagem_morreu, False, PRETO)
+tela_reiniciar.get_rect()
+
+pygame.display.set_caption('jogo_de_carro') #Este comando insere um nome ao jogo
 pygame_icon = pygame.image.load(os.path.join(diretorio_imagens, 'icon.png')).convert_alpha() #Este comando auxilia na exibição do icone do jogo
 pygame.display.set_icon(pygame_icon) #Este comando também auxilia nesta exibiçao
 
-mensagem_morreu = f'Você morreu, aperte R para reiniciar'
-tela_reiniciar = fonte.render(mensagem_morreu, False, PRETO)
-tela_reiniciar.get_rect()
+
+#SPRITES
+
+
+
+rua = pygame.image.load(os.path.join(diretorio_imagens, 'road.png')) #RUA
+rua = pygame.transform.scale(rua, ((LARGURA-300, int(ALTURA*5.36))))
+rua_rect = rua.get_rect()
+rua_rect.bottomleft = (300, ALTURA)
+rua_rect2 = rua_rect.copy()
+
+
+# MÚSICAS
+
+pygame.mixer.init()
+
+musica_partida = pygame.mixer.music.load(os.path.join(diretorio_sons, 'audios_convertidos/musica_partida.mp3'))
+musica_partida = pygame.mixer.music.set_volume(0.25)
+
+musica_menu = pygame.mixer.music.load(os.path.join(diretorio_sons, 'audios_convertidos/musica_menu.mp3'))
+musica_menu = pygame.mixer.music.set_volume(0.25)
+
+colisao_buraco = pygame.mixer.music.load(os.path.join(diretorio_sons, 'audios_convertidos/colisao_buracos.wav'))
+colisao_buraco = pygame.mixer.music.set_volume(0.35)
+
+colisao_carros = pygame.mixer.music.load(os.path.join(diretorio_sons, 'audios_convertidos/colisao_carros.wav'))
+colisão_carros = pygame.mixer.music.set_volume(0.35)
+
+colisao_troncos = pygame.mixer.music.load(os.path.join(diretorio_sons, 'audios_convertidos/colisao_troncos.wav'))
+colisao_troncos = pygame.mixer.music.set_volume(0.35)  
--- main.py
+++ main.py
@@ -2,31 +2,24 @@
 import pygame
 from pygame.locals import *   #Importa todas as funções e as constantes existentes no submódulo locals
 from sys import exit          #Essa função dentro do módulo sys torna possível fechar a janela
-import os
 import config as cf
 import carro_player as cp
 import carro_obstaculo as co
 import defs 
 
-
-def aumentar_contador(contador):
-    return contador + 1 
-
-
-def jogar(tela, relogio, player, rua, rua_rect, rua_rect2, carro, obstaculos):
-
-    contador = 0
+def jogar():
     cf.musica_partida = pygame.mixer.music.play()
+    player = pygame.sprite.Group() #GRUPOS
+    obstaculos = pygame.sprite.Group()
+    carro_player = cp.Carro_Player()
+    carro_obstaculo = co.Carro_Obstaculo()
+    player.add(carro_player)
+    obstaculos.add(carro_obstaculo)
+
     while cf.morreu == False: 
-        
-        """RODANDO = 0
-        PAUSADO = 1
-        jogo = RODANDO"""
         cf.tela.fill(cf.CINZA)
-        relogio.tick(cf.FPS*10)
-        pontuacao = f'{str(contador).zfill(2)}'
-        quadro_de_pontuacao = cf.fonte.render(pontuacao, True, cf.BRANCO, cf.PRETO)
-        """tela__de_pause = fonte.render('Aperte P para Continuar', False, PRETO, BRANCO)"""
+        cf.relogio.tick(cf.FPS*5)
+
 
         player.update() #Este comando vai atualizar frequente comandos a tela, auxiliando na fluidez do jogo
         obstaculos.update()
@@ -45,117 +38,89 @@ def jogar(tela, relogio, player, rua, rua_rect, rua_rect2, carro, obstaculos):
                     pygame.display.flip()
                     cf.morreu = True
 
-                """if event.key == K_p:
-                    if jogo != PAUSADO:
-                        jogo = PAUSADO
-                    else:
-                        jogo = RODANDO"""
-
-
                 if event.key == K_a:
                     if cf.carro_pos_x <= 355:
                         pass
                     else:
                         cf.carro_pos_x = cf.carro_pos_x - 100
-                        contador = aumentar_contador(contador)
-                        carro.movimento()
+                        cf.contador =+ 1
+                        carro_player.movimento()
                 if event.key == K_LEFT:
                     if cf.carro_pos_x == 355:
                         pass
                     else:
                         cf.carro_pos_x = cf.carro_pos_x - 100
-                        contador = aumentar_contador(contador)
-                        carro.movimento()
+                        cf.contador =+ 1
+                        carro_player.movimento()
                 if event.key == K_d:
                     if cf.carro_pos_x == 755:
                         pass
                     else:
                         cf.carro_pos_x = cf.carro_pos_x + 100
-                        contador = aumentar_contador(contador)
-                        carro.movimento()
+                        cf.contador =+ 1
+                        carro_player.movimento()
                 if event.key == K_RIGHT:
                     if cf.carro_pos_x == 755:
                         pass
                     else:
                         cf.carro_pos_x = cf.carro_pos_x + 100
-                        contador = aumentar_contador(contador)
-                        carro.movimento()
+                        cf.contador =+ 1
+                        carro_player.movimento()
                 if event.key == K_w:
                     if cf.carro_pos_y == 76:
                         pass
                     else:
                         cf.carro_pos_y = cf.carro_pos_y - 120
-                        contador = aumentar_contador(contador)
-                        carro.movimento()
+                        cf.contador =+ 1
+                        carro_player.movimento()
                 if event.key == K_UP:
                     if cf.carro_pos_y == 76:
                         pass
                     else:
                         cf.carro_pos_y = cf.carro_pos_y - 120
-                        contador = aumentar_contador(contador)
-                        carro.movimento()
+                        cf.contador =+ 1
+                        carro_player.movimento()
                 if event.key == K_s:
                     if cf.carro_pos_y == 556:
                         pass
                     else:
                         cf.carro_pos_y = cf.carro_pos_y + 120
-                        contador = aumentar_contador(contador)
-                        carro.movimento()
+                        cf.contador =+ 1
+                        carro_player.movimento()
                 if event.key == K_DOWN:
                     if cf.carro_pos_y == 556:
                         pass
                     else:
                         cf.carro_pos_y = cf.carro_pos_y + 120
-                        contador = aumentar_contador(contador)
-                        carro.movimento()
-            ''' if event.key == pygame.K_p:
-                    if jogo != PAUSADO:
-                        pygame.mixer.music.pause()
-                        pause = fonte_pause.render("PAUSE", True, AZUL, CINZA)
-                        tela.blit(pause, ((tela.get_width()-pause.get_width())/2, (tela.get_height()-pause.get_height())/2))
-                        jogo = PAUSADO
-                        
+                        cf.contador =+ 1
+                        carro_player.movimento()
 
-                    else:
-                        jogo = RODANDO
-                        pygame.mixer.music.unpause()
-                        
-                      
-        if jogo == PAUSADO:
-         pygame.display.flip()      ''' 
 
-        
-        """if jogo == PAUSADO:
-            pygame.display.flip()
-            continue"""
-        
         if cf.rua_numero == 0:
-            if rua_rect.topleft[1] >= 0:
-                rua_rect.bottomleft = (300, cf.ALTURA)
+            if cf.rua_rect.topleft[1] >= 0:
+                cf.rua_rect.bottomleft = (300, cf.ALTURA)
                 cf.rua_numero = 1
             else:
-                tela.blit(rua, rua_rect)
-                rua_rect.y += cf.FPS//6
+                cf.tela.blit(cf.rua, cf.rua_rect)
+                cf.rua_rect.y += cf.FPS//6
         else:
-            if rua_rect2.topleft[1] >= 0:
-                rua_rect2.bottomleft = (300, cf.ALTURA)
+            if cf.rua_rect2.topleft[1] >= 0:
+                cf.rua_rect2.bottomleft = (300, cf.ALTURA)
                 cf.rua_numero = 0
             else:
-                tela.blit(rua, rua_rect2)
-                rua_rect2.y += cf.FPS//6
+                cf.tela.blit(cf.rua, cf.rua_rect2)
+                cf.rua_rect2.y += cf.FPS//6
 
-        cf.tela.blit(quadro_de_pontuacao, (115,60))
-        player.draw(tela) #Este comando auxilia na exibição das sprites na tela
-        obstaculos.draw(tela)
-
-        #Essa função atualiza a tela do jogo a cada interação
-        pygame.display.flip()
+        cf.tela.blit(cf.quadro_de_pontuacao, (115,60))
+        player.draw(cf.tela) #Este comando auxilia na exibição das sprites na tela
+        obstaculos.draw(cf.tela)
+        pygame.display.flip()  #Essa função atualiza a tela do jogo a cada interação
 
 
-def tela_de_morte(relogio):   
+def tela_de_morte():   
 
     while cf.morreu: 
-        relogio.tick(cf.FPS*10)
+        cf.relogio.tick(cf.FPS*10)
         cf.tela.fill(cf.BRANCO)
         cf.musica_partida = pygame.mixer.music.stop()
         cf.tela.blit(cf.tela_reiniciar, (75, 280))
@@ -170,31 +135,11 @@ def tela_de_morte(relogio):
 
 
 def tela_jogo():
-    #Este comando controla a velocidade do objeto na tela
-    relogio = pygame.time.Clock() 
-    player = pygame.sprite.Group() #Este comando vai auxiliar, quando formos adicionar o carrinho na tela
-    obstaculos = pygame.sprite.Group()
-    carro_player = cp.Carro_Player()
-    carro_obstaculo = co.Carro_Obstaculo()
-    player.add(carro_player)
-    obstaculos.add(carro_obstaculo)
-
-    rua = pygame.image.load(os.path.join(cf.diretorio_imagens, 'road.png'))
-    rua = pygame.transform.scale(rua, ((cf.LARGURA-300, int(cf.ALTURA*5.36))))
-    rua_rect = rua.get_rect()
-    rua_rect.bottomleft = (300, cf.ALTURA)
-    rua_rect2 = rua_rect.copy()
-    """fonte_pause = pygame.font.Font(pygame.font.get_default_font(), 40)
-    Variáveis para pausar o jogo
-    RODANDO = 0
-    PAUSADO = 1
-    jogo = RODANDO 
-    """
     while True:
         if cf.morreu == False:
-            jogar(cf.tela, relogio, player, rua, rua_rect, rua_rect2, carro_player, obstaculos)
+            jogar()
         elif cf.morreu == True:
-            tela_de_morte(cf.tela, relogio)
+            tela_de_morte()
 
 try:
     tela_jogo()
